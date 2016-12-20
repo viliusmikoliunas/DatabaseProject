8df47f27d2c0dfa1823df522dc8cb89dc86b38ab
Vilius <Vilius@VILIUS-PC>
2016-12-21 00:00:54 +0200

    lasklsak

--- DatabaseProject/ConnectToDB.cs
+++ DatabaseProject/ConnectToDB.cs
@@ -8,22 +8,29 @@ using System.Data.SqlClient;
 
 namespace DatabaseProject
 {
+    /*
+     * fix warehouse table for warehouse purposes only
+       kiek laiko vairuotojas uztrunka nuvezti viena saldytuva
+     * 
+     */
     class ConnectToDB
     {
-        public DataTable GetTableFromDB(string tableName)
+        public DataTable GetTableFromDB(string selection, string location, string condition = "")
         {
-            string connectionStr = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=FridgeBussiness;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-            SqlConnection connection = new SqlConnection(connectionStr);
-            SqlDataAdapter adapter = new SqlDataAdapter("Select * from " + tableName, connection);
+            string connectionStr = 
+                @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=FridgeBussiness;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            string command = "Select " + selection + " from " + location;
+            command = string.IsNullOrEmpty(condition) ? command : command + " " + condition;
             DataTable table;
-            using (DataSet set = new DataSet("TempSet"))
+
+            using (var connection = new SqlConnection(connectionStr))
+            using (var adapter = new SqlDataAdapter(command, connection))
+            using (var set = new DataSet("TempSet"))
             {
-                adapter.FillSchema(set, SchemaType.Source, tableName);
-                adapter.Fill(set, tableName);
-                table = set.Tables[tableName];
+                adapter.FillSchema(set, SchemaType.Source, location);
+                adapter.Fill(set, location);
+                table = set.Tables[location];
             }
-            connection.Close();
-            connection.Dispose();
             return table;
         }
     }
@@ -32,45 +39,40 @@ namespace DatabaseProject
     {
         public DateTime DateParsing(string yy, string mm, string dd)
         {
-            int year = Int32.Parse(yy);
-            int month = Int32.Parse(mm);
-            int day = Int32.Parse(dd);
+            int year = int.Parse(yy);
+            int month = int.Parse(mm);
+            int day = int.Parse(dd);
             return new DateTime(year, month, day);
         }
     }
 
     class DriverTable
     {
-        public DataTable GenerateDataTableForDriver()
+        public DataTable GetDriverTable(string driverFullName)
         {
-            DataTable newTable = new DataTable();
-            newTable.Columns.AddRange(new []
+            DataTable newTable = GenerateEmptyDriverTable();
+            using (var connection = new FridgeBussinessEntities2())
             {
-                new DataColumn("FridgeId",typeof(int)),
-                new DataColumn("Client",typeof(string)), 
-                new DataColumn("Address",typeof(string)),
-                new DataColumn("DeliverUntil",typeof(DateTime)),
-                new DataColumn("IsDelivered",typeof(bool))
-            });
-            for (int i = 0; i < newTable.Columns.Count - 1; i++) newTable.Columns[i].ReadOnly = true;
-            using (var conn = new FridgeBussinessEntities2())
-            {
-                DataRow newnew = newTable.NewRow();
-                var driver =
-                    from dd in conn.Driver
-                    join ff in conn.Fridge on dd.PersonalCode equals ff.DeliveringDriverPersonalCode
-                    select new {drv = dd.FirstName+" "+dd.LastName, FridgeId = ff.FridgeID,
-                        cust = ff.Customer, until = ff.DeliverUntil, delivered = (ff.DeliveredAt!=null)}
-                    ;
-                foreach (var car in driver)
+                var driverTable =
+                    from dd in connection.Driver
+                    join ff in connection.Fridge on dd.PersonalCode equals ff.DeliveringDriverPersonalCode
+                    select new
+                    {
+                        driver = dd.FirstName + " " + dd.LastName,
+                        FridgeId = ff.FridgeID,
+                        cust = ff.Customer,
+                        until = ff.DeliverUntil,
+                        delivered = ff.DeliveredAt
+                    };
+                foreach (var car in driverTable)
                 {
-                    if (car.drv.Equals("Vasia Jonavicius")&&!car.delivered)
+                    if (car.driver.Equals(driverFullName) && car.delivered==null)
                     {
                         DataRow newRow = newTable.NewRow();
-                        newRow[0] = car.FridgeId;
-                        newRow[1] = car.cust;
-                        newRow[2] = conn.Customer.FirstOrDefault(c => c.CustomerName == car.cust).Address;
-                        newRow[3] = car.until;
+                        newRow["FridgeId"] = car.FridgeId;
+                        newRow["Client"] = car.cust;
+                        newRow["Address"] = connection.Customer.FirstOrDefault(c => c.CustomerName == car.cust).Address;
+                        newRow["DeliverUntil"] = car.until;
                         newTable.Rows.Add(newRow);
                     }
                 }
@@ -78,10 +80,18 @@ namespace DatabaseProject
             return newTable;
         }
 
-        public DataTable FillDataTable(DataTable table)
+        private DataTable GenerateEmptyDriverTable()
         {
-
-            return table;
+            DataTable newTable = new DataTable();
+            newTable.Columns.AddRange(new[]
+            {
+                new DataColumn("FridgeId",typeof(int)),
+                new DataColumn("Client",typeof(string)),
+                new DataColumn("Address",typeof(string)),
+                new DataColumn("DeliverUntil",typeof(DateTime)),
+                new DataColumn("Delivered",typeof(string))
+            });
+            return newTable;
         }
     }
 }
--- DatabaseProject/DriverForm.cs
+++ DatabaseProject/DriverForm.cs
@@ -13,27 +13,77 @@ namespace DatabaseProject
     public partial class DriverForm : Form
     {
         private DataTable driverTable;
-        public DriverForm()
+        private readonly List<int> deliveredFridgesTodayIdList = new List<int>();
+        private string driverFullName;
+        public DriverForm(string currentDriver="")
         {
             InitializeComponent();
+            driverFullName = currentDriver;
         }
 
         private void DriverForm_Load(object sender, EventArgs e)
         {
             DriverTable getTable = new DriverTable();
-            driverTable = getTable.GenerateDataTableForDriver();
-            driverTable = getTable.FillDataTable(driverTable);
+            driverTable = getTable.GetDriverTable(driverFullName);
 
             driverGridView.DataSource = driverTable;
         }
 
         private void DriverForm_FormClosing(object sender, FormClosingEventArgs e)
         {
+            if (deliveredFridgesTodayIdList.Count > 0)
+            {
+                string line = "";
+                using (var connection = new FridgeBussinessEntities2())
+                {
+                    foreach (int id in deliveredFridgesTodayIdList)
+                    {
+                        Fridge temp = new Fridge();
+                        temp = connection.Fridge.Single(f => f.FridgeID == id);
+                        line += "Klientas: " + temp.Customer + " | Saldytuvo nr: " + temp.FridgeID + "\n";
+                    }
+                }
 
+                DialogResult result = MessageBox.Show("Pakeitimai:\n"+line+"Issaugoti?", "Warning",
+                MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning);
+                if (result == DialogResult.Yes)
+                {
+                    using (var connection = new FridgeBussinessEntities2())
+                    {
+                        foreach (var fr in deliveredFridgesTodayIdList) connection.Fridge.Single(f => f.FridgeID == fr).DeliveredAt = DateTime.Today;
+                        connection.SaveChanges();
+                    }
+                }
+                else if (result == DialogResult.Cancel)
+                {
+                    e.Cancel = true;
+                }
+            }
         }
 
         private void DriverForm_FormClosed(object sender, FormClosedEventArgs e)
         {
+
+        }
+
+        private void deliveredButton_Click(object sender, EventArgs e)
+        {
+            int currRowFrId = (int)driverGridView.CurrentRow.Cells[0].Value;
+            if (string.IsNullOrEmpty(driverGridView.CurrentRow.Cells["Delivered"].Value as string))
+            {
+                deliveredFridgesTodayIdList.Add(currRowFrId);
+                driverGridView.CurrentRow.Cells["Delivered"].Value = DateTime.Today;
+            }
+        }
+
+        private void unmarkButton_Click(object sender, EventArgs e)
+        {
+            int currRowFrId = (int)driverGridView.CurrentRow.Cells[0].Value;
+            if (!string.IsNullOrEmpty(driverGridView.CurrentRow.Cells["Delivered"].Value as string))
+            {
+                deliveredFridgesTodayIdList.Remove(currRowFrId);
+                driverGridView.CurrentRow.Cells["Delivered"].Value = new DateTime();
+            }
 
         }
     }
